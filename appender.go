c871d5697d0610b9cc9dc972e0ccbca1815273e9
Child of the Universe <childoftheuniverse@protonmail.com>
2018-06-10 18:18:15 +0200

    Add Prometheus latency and error counters.
    We don't add request counters because we already get the total number of
    requests via the latency histogram + error counter.

--- appender.go
+++ appender.go
@@ -1,11 +1,35 @@
 package rados
 
 import (
+	"context"
 	"github.com/ceph/go-ceph/rados"
 	"github.com/childoftheuniverse/filesystem"
-	"golang.org/x/net/context"
+	"github.com/prometheus/client_golang/prometheus"
 	"os"
+	"time"
 )
+
+var radosAppenderLatencies = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Subsystem: "rados",
+	Name:      "append_latency",
+	Help:      "Latency of Rados Append requests",
+	Buckets:   prometheus.ExponentialBuckets(0.001, 5, 20),
+}, []string{"pool"})
+var radosAppenderErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "append_errors",
+	Help:      "Number of errors received when appending to Rados files",
+}, []string{"pool"})
+var radosAppenderBytes = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "append_bytes",
+	Help:      "Number of bytes sent when appending to Rados files",
+}, []string{"pool"})
+
+func init() {
+	prometheus.MustRegister(radosAppenderLatencies)
+	prometheus.MustRegister(radosAppenderErrors)
+}
 
 /*
 Appender provides a WriteCloser API for appending data to Rados objects.
@@ -15,6 +39,7 @@ Seeks are supported, but only as a means to determine the current position.
 */
 type Appender struct {
 	rctx *rados.IOContext
+	pool string
 	oid  string
 	pos  int64
 }
@@ -25,8 +50,16 @@ the specified oid.
 */
 func NewAppender(rctx *rados.IOContext, oid string) (*Appender, error) {
 	var stat rados.ObjectStat
+	var pool string
 	var pos int64
 	var err error
+
+	/*
+	   Determine the name of the pool the object resides in, for prometheus.
+	*/
+	if pool, err = rctx.GetPoolName(); err != nil {
+		return nil, err
+	}
 
 	/*
 	   Determine the size of the object. If this fails, assume the object doesn't
@@ -38,6 +71,7 @@ func NewAppender(rctx *rados.IOContext, oid string) (*Appender, error) {
 
 	return &Appender{
 		rctx: rctx,
+		pool: pool,
 		oid:  oid,
 		pos:  pos,
 	}, nil
@@ -50,10 +84,19 @@ as complete Write() calls.
 TODO: does not respect contexts yet.
 */
 func (w *Appender) Write(ctx context.Context, p []byte) (int, error) {
+	var start = time.Now()
 	var err error
+
 	if err = w.rctx.Append(w.oid, p); err != nil {
+		radosAppenderErrors.With(prometheus.Labels{"pool": w.pool}).Inc()
 		return 0, err
 	}
+
+	radosAppenderLatencies.With(prometheus.Labels{"pool": w.pool}).Observe(
+		time.Now().Sub(start).Seconds())
+	radosAppenderBytes.With(prometheus.Labels{"pool": w.pool}).Add(
+		float64(len(p)))
+
 	w.pos += int64(len(p))
 	return len(p), nil
 }
--- read_writer.go
+++ read_writer.go
@@ -1,11 +1,55 @@
 package rados
 
 import (
+	"context"
 	"github.com/ceph/go-ceph/rados"
-	"golang.org/x/net/context"
+	"github.com/prometheus/client_golang/prometheus"
 	"io"
 	"os"
+	"time"
 )
+
+var radosReadLatencies = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Subsystem: "rados",
+	Name:      "read_latency",
+	Help:      "Latency of Rados Read requests",
+	Buckets:   prometheus.ExponentialBuckets(0.001, 5, 20),
+}, []string{"pool"})
+var radosWriteLatencies = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Subsystem: "rados",
+	Name:      "write_latency",
+	Help:      "Latency of Rados Write requests",
+	Buckets:   prometheus.ExponentialBuckets(0.001, 5, 20),
+}, []string{"pool"})
+var radosReadErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "read_errors",
+	Help:      "Number of errors received when reading from Rados files",
+}, []string{"pool"})
+var radosWriteErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "write_errors",
+	Help:      "Number of errors received when writing to Rados files",
+}, []string{"pool"})
+var radosReadBytes = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "read_bytes",
+	Help:      "Number of bytes received when reading from Rados files",
+}, []string{"pool"})
+var radosWriteBytes = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Subsystem: "rados",
+	Name:      "write_bytes",
+	Help:      "Number of bytes sent when writing to Rados files",
+}, []string{"pool"})
+
+func init() {
+	prometheus.MustRegister(radosReadLatencies)
+	prometheus.MustRegister(radosWriteLatencies)
+	prometheus.MustRegister(radosReadErrors)
+	prometheus.MustRegister(radosWriteErrors)
+	prometheus.MustRegister(radosReadBytes)
+	prometheus.MustRegister(radosWriteBytes)
+}
 
 /*
 ReadWriteCloser provides both a ReadCloser and a WriteCloser for Rados objects.
@@ -14,6 +58,7 @@ a regular filesystem API.
 */
 type ReadWriteCloser struct {
 	rctx *rados.IOContext
+	pool string
 	oid  string
 	pos  int64
 }
@@ -28,8 +73,16 @@ guarantee that the object can actually be accessed properly. This will only
 be determined on the first call to Read() or Write().
 */
 func NewReadWriteCloser(rctx *rados.IOContext, oid string) *ReadWriteCloser {
+	var pool string
+
+	/*
+	   Determine the name of the pool the object resides in, for prometheus.
+	*/
+	pool, _ = rctx.GetPoolName()
+
 	return &ReadWriteCloser{
 		rctx: rctx,
+		pool: pool,
 		oid:  oid,
 		pos:  0,
 	}
@@ -41,12 +94,21 @@ specified buffer. Returns the number of bytes actually read.
 TODO: does not respect contexts yet.
 */
 func (r *ReadWriteCloser) Read(ctx context.Context, p []byte) (n int, err error) {
+	var start = time.Now()
 	n, err = r.rctx.Read(r.oid, p, uint64(r.pos))
 	if n > 0 {
 		r.pos += int64(n)
 	} else if n == 0 && err == nil {
 		/* TODO: find some way to check this is actually the end of the file. */
 		err = io.EOF
+	}
+	if err == nil {
+		radosReadLatencies.With(prometheus.Labels{"pool": r.pool}).Observe(
+			time.Now().Sub(start).Seconds())
+		radosReadBytes.With(prometheus.Labels{"pool": r.pool}).Add(
+			float64(n))
+	} else {
+		radosReadErrors.With(prometheus.Labels{"pool": r.pool}).Inc()
 	}
 	return
 }
@@ -57,12 +119,19 @@ object specified by oid.
 TODO: does not respect contexts yet.
 */
 func (r *ReadWriteCloser) Write(ctx context.Context, p []byte) (int, error) {
+	var start = time.Now()
 	var err = r.rctx.Write(r.oid, p, uint64(r.pos))
-	if err == nil {
-		r.pos += int64(len(p))
-		return len(p), nil
+	if err != nil {
+		radosWriteErrors.With(prometheus.Labels{"pool": r.pool}).Inc()
+		return 0, err
 	}
-	return 0, err
+
+	radosWriteLatencies.With(prometheus.Labels{"pool": r.pool}).Observe(
+		time.Now().Sub(start).Seconds())
+	radosWriteBytes.With(prometheus.Labels{"pool": r.pool}).Add(
+		float64(len(p)))
+	r.pos += int64(len(p))
+	return len(p), nil
 }
 
 /*
